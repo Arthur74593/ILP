08e0c277a8ce76e320bb54fdf391053475170db7
Arthur74593 <arthurvinicius_10@outlook.com>
2026-04-10 00:01:37 +0000

    mensagem

---
+++ lista/q24.py
@@ -0,0 +1,14 @@
+A = 4
+B = 10
+C = 4
+D = True
+D2 = False
+E = False
+F = 0
+G = -3
+I = [1, 2, 3, 4, 5]
+
+1<2 == True
+print(1<2 == True )
+# O resultado sera false pois o true é considerado como 1 e o progama le a expressão assim: 1<2 and True==2
+# porém, True==2 é false logo, pela presença do "and" (False, True) ele considera tudo False
---
+++ lista/q25.py
@@ -0,0 +1,18 @@
+A = 4
+B = 10
+C = 4
+D = True
+D2 = False
+E = False
+F = 0
+G = -3
+I = [1, 2, 3, 4, 5]
+
+print((A%2 == 0) and (B>2*A) and (-5 <= G <= -1) and D)
+
+
+
+# A é par
+# B é maior que o dobro de A
+# G está no intervalo [-5, -1] (inclusive)
+# D é verdadeiro

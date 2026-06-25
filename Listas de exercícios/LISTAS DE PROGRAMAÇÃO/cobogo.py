f50d3753a203fc3a7a8ab1ba0e89ec950b8a3344
Arthur74593 <arthurvinicius_10@outlook.com>
2026-06-25 14:52:27 +0000

    fsfvjks

--- Listas de exercícios/LISTAS DE PROGRAMAÇÃO/cobogo.py
+++ Listas de exercícios/LISTAS DE PROGRAMAÇÃO/cobogo.py
@@ -1,22 +1,7 @@
-lista = list(map(int, input("Digite os números separados por espaço: ").split()))
+numero = int(input("Quantidade de números na lista:"))
+lista = list(map(int, input("Digite o números separados por espaço:").split()))
 
-contagem = {}
+lista_nova = set(lista)
+mais_uma = list(lista_nova)
 
-for num in lista:
-    if num in contagem:
-        contagem[num] += 1
-    else:
-        contagem[num] = 1
-
-menor_freq = float('inf')  
-menos_repete = None
-
-for num in contagem:
-    if contagem[num] < menor_freq:
-        menor_freq = contagem[num]
-        menos_repete = num
-    elif contagem[num] == menor_freq:
-        if menos_repete is None or num < menos_repete:
-            menos_repete = num
-
-print(menos_repete)
+print(*mais_uma)
